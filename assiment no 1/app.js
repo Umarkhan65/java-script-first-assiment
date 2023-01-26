dc28d5002433fea4b373cf335ec14547c907e58f
Umarkhan65 <122873840+Umarkhan65@users.noreply.github.com>
2023-01-26 07:04:20 -0800

    Add files via upload

---
+++ assiment no 1/app.js
@@ -0,0 +1,19 @@
+// chapter no=1
+// Question no1
+// alert ("Hello sir!");
+
+// Question no-2
+// var name = ("Umar Khan  \n  Aamir Khan") ;
+// alert (name)
+
+// // Question no=3
+// alert("Well come to js land....")
+
+// Question no=4
+// var code= ("Happy coding! \n prevent this page from creating addition dialogs")
+// alert(code)
+
+// Question no=5
+// alert(
+//     "Hello..... i can run js throught my wed browser's consol"
+// )
